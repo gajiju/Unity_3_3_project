72e572a4cd8d87e5637e48a060d3402c14a515b5
gajiju <rladydtn730@naver.com>
2024-01-07 03:49:36 +0900

    Player_ver7
    플레이어 공격 피격 구현

--- Assets/02.kys/Scripts/Player/PlayerStats_Kys.cs
+++ Assets/02.kys/Scripts/Player/PlayerStats_Kys.cs
@@ -8,6 +8,7 @@ using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Playables;
+using UnityEngine.ProBuilder;
 using UnityEngine.ProBuilder.MeshOperations;
 
 [Serializable]
@@ -32,18 +33,27 @@ public class PlayerStats_Kys : MonoBehaviour
     public InputAction PlayerMove; //플레이어 컨트롤러
     [SerializeField] float _speed = 10.0f; //이동속도
 
+    bool iswall = false; //벽체크용
     #endregion
 
     
     float _Radio = 0;
-
+    #region 공격범위 관련
+    #endregion
     #region 점프 구현
     [SerializeField]float JumpPower = 10.0f;
     private Rigidbody rigid;
     bool IsJump = false;
     #endregion
+    #region 공격 관련
+    float fireDelay; //공격 딜레이
+    bool isAttackReady; //딜레이 후 조건
+    public GameObject weapon;
 
-
+    #endregion
+    #region 피격관련
+    Material mat;
+    #endregion
 
     public enum Player_State
     {
@@ -58,13 +68,14 @@ public class PlayerStats_Kys : MonoBehaviour
     }
 
     Player_State State = Player_State.Idle;
-
+    SpriteRenderer spriteRenderer;
 
 
     public void Start()
     {
         GameManager gmg = GameManager.Instance();
         rigid = GetComponent<Rigidbody>();
+        mat = gameObject.GetComponent<MeshRenderer>().material;
 
         GameManager.Input.KeyAction -= OnMove;
         GameManager.Input.KeyAction += OnMove;
@@ -73,11 +84,13 @@ public class PlayerStats_Kys : MonoBehaviour
         GameManager.Input.KeyAction -= OnAttack;
         GameManager.Input.KeyAction += OnAttack;
         
+        
     }
 
     public void Update()
     {
-        if(userdata.Player_CurrentHp == 0)
+        
+        if (userdata.Player_CurrentHp == 0)
         {
             State = Player_State.Die;
             OnDie();
@@ -108,6 +121,7 @@ public class PlayerStats_Kys : MonoBehaviour
     {
         if (userdata.Player_CurrentHp == 0)
             return;
+        StopWall();
     }
 
     #region 대기
@@ -132,23 +146,44 @@ public class PlayerStats_Kys : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.5f);
-            transform.position += Vector3.left * Time.deltaTime * _speed;
+            if(iswall)
+            {
+                transform.position += Vector3.left * Time.deltaTime;
+            }
+            else
+            {
+                transform.position += Vector3.left * Time.deltaTime * _speed;
+            }
             _Radio = Mathf.Lerp(_Radio, 1, 10.0f * Time.deltaTime);
             ani.SetFloat("Idle_Run_Radio", _Radio);
         }
         else if (Input.GetKey(KeyCode.D))
         {
+
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.5f);
-            transform.position += Vector3.right * Time.deltaTime * _speed;
+            if (iswall)
+            {
+                transform.position += Vector3.right * Time.deltaTime;
+            }
+            else
+            {
+                transform.position += Vector3.right * Time.deltaTime * _speed;
+            }
             _Radio = Mathf.Lerp(_Radio, 1, 10.0f * Time.deltaTime);
             ani.SetFloat("Idle_Run_Radio", _Radio);
-
         }
         else
         {
             State = Player_State.Idle;
         }
 
+    }
+
+    void StopWall() //충돌 확인용
+    {
+        
+        //Debug.DrawRay(transform.position, transform.forward, Color.green); 
+        iswall = Physics.Raycast(transform.position, transform.forward,1, LayerMask.GetMask("Wall")) ;
     }
     #endregion
     #region 점프
@@ -188,8 +223,6 @@ public class PlayerStats_Kys : MonoBehaviour
         {
             if (State == Player_State.Attack)
             {
-                MonsterAttack monster = GetComponent<MonsterAttack>();
-
 
             }
             else if ((State == Player_State.Idle || State == Player_State.Move)) //피격
@@ -200,14 +233,26 @@ public class PlayerStats_Kys : MonoBehaviour
     }
 
 
-
     #region 공격
     public void OnAttack()
     {
-        Animator ani = GetComponent<Animator>();
         if (Input.GetMouseButtonDown(0))
         {
+
+            Player_Weapon_kys eqweapon = weapon.GetComponent<Player_Weapon_kys>();
+            /* 공격 딜레이 구현
+            fireDelay = Time.deltaTime;
+            isAttackReady = eqweapon.AttackSpeed < fireDelay;
+            */
+            Animator ani = GetComponent<Animator>();
+            State = Player_State.Attack;
+
+            if (State == Player_State.Attack)
+            {
+                eqweapon.Use();
                 ani.SetTrigger("Attack");
+                fireDelay = 0;
+            }
         }
     }
     public void OnAttackOnOff()
@@ -215,6 +260,7 @@ public class PlayerStats_Kys : MonoBehaviour
         if(State == Player_State.Idle || State == Player_State.Move || State == Player_State.Jump)
         {
             State = Player_State.Attack;
+
         }
         else if(State == Player_State.Attack)
         {
@@ -249,6 +295,8 @@ public class PlayerStats_Kys : MonoBehaviour
     #region 피격
     public void OnPain()
     {
+        Monster_Kys monster = new Monster_Kys();
+
         Animator ani = GetComponent<Animator>();
         if (State == Player_State.Die)
             return;
@@ -257,18 +305,34 @@ public class PlayerStats_Kys : MonoBehaviour
             Debug.Log("아야");
             ani.SetTrigger("Pain");
 
-            /*
             if(userdata.Player_CurrentHp > 0)
             {
-            userdata.Player_CurrentHp -= Monster_kys.Damage;
-             State = Player_State.Idle;
+                userdata.Player_CurrentHp -= monster.Monster_Attack;
+                StartCoroutine(OnPainOn());
+                Debug.Log($" 받은 데미지{monster.Monster_Attack} 현제 체력 {userdata.Player_CurrentHp}");
+                State = Player_State.Idle;
             }
             if(userdata.Player_CurrentHp <= 0)
             {
             userdata.Player_CurrentHp = 0;
             State = Player_State.Die;
             }
-            */
+            
+        }
+    }
+
+    IEnumerator OnPainOn()
+    {
+        mat.color = Color.red;
+        yield return new WaitForSeconds(0.5f);
+
+        if (userdata.Player_CurrentHp > 0)
+        {
+            mat.color = Color.white;
+        }
+        else
+        {
+            mat.color = Color.gray;
         }
     }
     #endregion
@@ -276,6 +340,7 @@ public class PlayerStats_Kys : MonoBehaviour
     #region 죽음
     public void OnDie()
     {
+        
         Animator ani = GetComponent<Animator>();
         State = Player_State.Die;
         ani.SetTrigger("Die");
