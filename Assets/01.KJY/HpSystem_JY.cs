2d2d39cac0b907b8365698590cb959a76c88cc0c
FrostKim0208 <admiral0208@naver.com>
2024-01-08 11:38:11 +0900

    [Feat] splint

--- Assets/01.KJY/HpSystem_JY.cs
+++ Assets/01.KJY/HpSystem_JY.cs
@@ -10,12 +10,7 @@ public class HpSystem_JY : MonoBehaviour
     [SerializeField] private float HpChangeDelay = .5f;
 
     private PlayerStatsHandler_JY _statsHandler;
-    private float _timeSinceLastChange = float.MaxValue;
 
-    public event Action OnDamage;
-    public event Action OnHeal;
-    public event Action OnDeath;
-    public event Action OnInvincibilityEnd;
     public Image uiBar;
 
     public float CurrentHp { get; private set; }
@@ -34,50 +29,10 @@ public class HpSystem_JY : MonoBehaviour
 
     private void Update()
     {
-        if (_timeSinceLastChange < HpChangeDelay)
-        {
-            _timeSinceLastChange += Time.deltaTime;
-            if (_timeSinceLastChange >= HpChangeDelay)
-            {
-                OnInvincibilityEnd?.Invoke();
-            }
-        }
+
         uiBar.fillAmount = GetPercentage();
     }
 
-    public bool ChangeHealth(float change)
-    {
-        if (change == 0 || _timeSinceLastChange < HpChangeDelay)
-        {
-            return false;
-        }
-
-        _timeSinceLastChange = 0f;
-        CurrentHp += change;
-        CurrentHp = CurrentHp > MaxHp ? MaxHp : CurrentHp;
-        CurrentHp = CurrentHp < 0 ? 0 : CurrentHp;
-
-        if (change > 0)
-        {
-            OnHeal?.Invoke();
-        }
-        else
-        {
-            OnDamage?.Invoke();
-        }
-
-        if (CurrentHp <= 0f)
-        {
-            CallDeath();
-        }
-
-        return true;
-    }
-
-    private void CallDeath()
-    {
-        OnDeath?.Invoke();
-    }
     public float GetPercentage()
     {
         return CurrentHp / MaxHp;
